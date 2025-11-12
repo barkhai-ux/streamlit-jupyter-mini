810e63fbc55b6c67be977717cb617f7686a6c9e9
Arkhai Batsuuri <b.arkhai@aum.edu.mn>
2025-11-12 21:32:54 +0800

    Remove footer rendering from app.py imports

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from config import CUSTOM_CSS, setup_page_config, initialize_session_state
-from ui import render_header, render_sidebar, render_main_content, render_footer
+from ui import render_header, render_sidebar, render_main_content
 
 st.markdown(CUSTOM_CSS, unsafe_allow_html=True)
 
